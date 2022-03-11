41a60da89539e9178a102682b214b2e37be10dfb
Dev-Ismael <dev.ismaeel@gmail.com>
2022-03-11 21:59:25 +0200

    finish account header

--- resources/views/web/account.blade.php
+++ resources/views/web/account.blade.php
@@ -4,11 +4,25 @@
 
     <div id="account-page">
 
-        Account Page
+
+        <!----- Header ----->
+        <div id="header" class="bg-parallax">
+            <div class="overlay"></div>   
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-10 offset-md-1">
+                        <h3> 
+                            Hi, Abdulrahman!
+                            <i class="fa-solid fa-face-grin-wide" style="color:#ffe817"></i>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+
 
         
-
-
 
 
         
