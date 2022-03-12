3078fa8a6b33997884b5995d7ba9ef0fc6480042
Dev-Ismael <dev.ismaeel@gmail.com>
2022-03-12 23:12:07 +0200

    create user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,31 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 55);
+            $table->string('email' , 55)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone' , 100 );
+            $table->string('balance', 10);
+            $table->tinyInteger('role');
             $table->rememberToken();
             $table->timestamps();
         });
     }
+
+
+    // $table->id();
+    // $table->string('name', 255);
+    // $table->string('email', 255)->unique();
+    // $table->timestamp('email_verified_at')->nullable();
+    // $table->string('password');
+    // $table->string('phone' , 100 );
+    // $table->integer('balance' , 9 );
+    // $table->tinyInteger('role');    // 1=> admin  ,  2=> affiliate  ,  3=> user
+    // $table->rememberToken();
+    // $table->timestamps();
+
+
 
     /**
      * Reverse the migrations.
