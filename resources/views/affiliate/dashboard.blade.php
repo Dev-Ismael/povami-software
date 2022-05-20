834da77ee29831ee13bc4bcf845dfa987241ed99
Abdulrahman Ismael <64872640+Dev-Ismael@users.noreply.github.com>
2022-05-21 00:41:17 +0200

    create withdrawals money design

--- resources/views/affiliate/dashboard.blade.php
+++ resources/views/affiliate/dashboard.blade.php
@@ -37,40 +37,37 @@
 
                     <!----------- Tabs nav ---------->
                     <div class="col-md-3">
-                        <div class="nav flex-column nav-pills nacustom" id="tab" role="tablist"
-                            aria-orientation="vertical">
+                        <div class="nav flex-column nav-pills nacustom" id="tab" role="tablist" aria-orientation="vertical">
 
 
                             <a class="nav-link mb-3 p-3 shadow active" id="statistics-tab" data-toggle="pill"
-                                href="#statistics" role="tab" aria-controls="statistics"
-                                aria-selected="false">
+                                href="#statistics" role="tab" aria-controls="statistics" aria-selected="false">
                                 <i class="fa-solid fa-gauge pr-1"></i>
                                 <span class="font-weight-bold small text-uppercase">Statistics</span>
                             </a>
 
-                            <a class="nav-link mb-3 p-3 shadow" id="tools-tab" data-toggle="pill"
-                                href="#tools" role="tab" aria-controls="tools" aria-selected="false">
+                            <a class="nav-link mb-3 p-3 shadow" id="tools-tab" data-toggle="pill" href="#tools" role="tab"
+                                aria-controls="tools" aria-selected="false">
                                 <i class="fa-solid fa-screwdriver-wrench pr-1"></i>
                                 <span class="font-weight-bold small text-uppercase">My Tools</span>
                             </a>
 
-                            <a class="nav-link mb-3 p-3 shadow" id="credits-tab" data-toggle="pill"
-                                href="#credits" role="tab" aria-controls="credits" aria-selected="false">
-                                <i class="fa-solid fa-money-check-dollar pr-1"></i>
-                                <span class="font-weight-bold small text-uppercase">Povami Credits</span>
+                            <a class="nav-link mb-3 p-3 shadow" id="withdrawals-tab" data-toggle="pill" href="#withdrawals"
+                                role="tab" aria-controls="withdrawals" aria-selected="false">
+                                <i class="fa-solid fa-money-bill-1 pr-1"></i>
+                                <span class="font-weight-bold small text-uppercase">withdrawals Money</span>
                             </a>
 
 
                             <a class="nav-link mb-3 p-3 shadow" id="presonal-info-tab" data-toggle="pill"
-                                href="#presonal-info" role="tab" aria-controls="presonal-info"
-                                aria-selected="true">
+                                href="#presonal-info" role="tab" aria-controls="presonal-info" aria-selected="true">
                                 <i class="fa fa-user-circle-o mr-2"></i>
                                 <span class="font-weight-bold small text-uppercase">Personal information</span>
                             </a>
 
 
                             <a class="nav-link mb-3 p-3 shadow logout" aria-selected="false" onclick="event.preventDefault();
-                                                                    document.getElementById('logout-form').submit();">
+                                                                        document.getElementById('logout-form').submit();">
                                 <i class="fa-solid fa-door-open pr-2"></i>
                                 <span class="font-weight-bold small text-uppercase">logout</span>
                             </a>
@@ -109,8 +106,8 @@
 
 
                             <!----------- statistics Section ------------->
-                            <div class="tab-pane fade shadow rounded bg-white show active" id="statistics"
-                                role="tabpanel" aria-labelledby="statistics-tab">
+                            <div class="tab-pane fade shadow rounded bg-white show active" id="statistics" role="tabpanel"
+                                aria-labelledby="statistics-tab">
                                 <h4 class="font-italic mb-3">
                                     <i class="fa-solid fa-gauge"></i>
                                     Statistics
@@ -266,8 +263,8 @@
 
 
                             <!----------- statistics Section ------------->
-                            <div class="tab-pane fade shadow rounded bg-white" id="tools"
-                                role="tabpanel" aria-labelledby="tools-tab">
+                            <div class="tab-pane fade shadow rounded bg-white" id="tools" role="tabpanel"
+                                aria-labelledby="tools-tab">
                                 <h4 class="font-italic mb-3">
                                     <i class="fa-solid fa-screwdriver-wrench"></i>
                                     My Tools
@@ -276,12 +273,15 @@
                                 <div class="coupon-container pt-3">
                                     <ul>
                                         <li>
-                                            Share your coupon code with clients this will provide clients more discounts at our services ,
+                                            Share your coupon code with clients this will provide clients more discounts at
+                                            our services ,
                                             you’ll be more likely to convince your target audience to make a purchase.
                                         </li>
                                         <li>
-                                            We will know that the client is recommended for our services through you by using the discount coupon,
-                                            so be sure to provide the client with the coupon so that you get your percentage of the profits
+                                            We will know that the client is recommended for our services through you by
+                                            using the discount coupon,
+                                            so be sure to provide the client with the coupon so that you get your percentage
+                                            of the profits
                                         </li>
                                         <li>
                                             To know more info about commission system <a href="#">Click Here</a>
@@ -291,7 +291,7 @@
                                         <div class="card">
                                             <div class="main">
                                                 <div class="co-img">
-                                                    <img src="{{ asset("images/favicon.png") }}" alt="povami-icon"/>
+                                                    <img src="{{ asset('images/favicon.png') }}" alt="povami-icon" />
                                                 </div>
                                                 <div class="vertical"></div>
                                                 <div class="content">
@@ -316,16 +316,19 @@
                                 <div class="referral-container  pt-3">
                                     <ul>
                                         <li>
-                                            Also you can use referral link to invite other affiliators like you for signup at POVAMI affiliator system and
+                                            Also you can use referral link to invite other affiliators like you for signup
+                                            at POVAMI affiliator system and
                                             work in marketing and attracting clients...
                                             This will make you have many affiliators in your network
                                         </li>
                                         <li>
-                                            Affiliators in your network will working to get clients and without any interference from you in the process you will get 1% for every complete service
+                                            Affiliators in your network will working to get clients and without any
+                                            interference from you in the process you will get 1% for every complete service
                                         </li>
                                     </ul>
                                     <div class="input-group mb-3">
-                                        <input type="text" class="form-control" value="http://povamisoftware.co/affiliate/dashboard#v-pills-toolshttp://povamisoftware.co/affiliate/dashboard#v-pills-tools" readonly/>
+                                        <input type="text" class="form-control"
+                                            value="http://povamisoftware.co/affiliate/dashboard#v-pills-tools" readonly />
                                         <div class="input-group-append copybtn">
                                             <button class="input-group-text d-flex justify-content-center">
                                                 <i class="fa-solid fa-copy pr-1"></i>
@@ -343,10 +346,118 @@
 
 
 
+
+
+
+
+
+
+
+
+
+                            <!----------- statistics Section ------------->
+                            <div class="tab-pane fade shadow rounded bg-white" id="withdrawals" role="tabpanel"
+                                aria-labelledby="withdrawals-tab">
+                                <h4 class="font-italic mb-3">
+                                    <i class="fa-solid fa-money-bill-1 pr-1"></i>
+                                    Withdrawals Money
+                                </h4>
+
+                                <!---------------- withdrawals Table ------------------>
+                                <div class="affiliator-list">
+                                    <div class="table-header  mt-5 mb-2 d-flex">
+                                        <h5 class="font-italic">
+                                            <i class="fa-solid fa-list-check"></i>
+                                            Your withdrawal requests
+                                        </h5>
+                                        <button class="btn purple ml-auto mb-2" data-toggle="modal" data-target="#createRequestModal">
+                                            <i class="fa-solid fa-paper-plane"></i> Request
+                                        </button>
+                                    </div>
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Amount</th>
+                                                <th scope="col">Payment Method</th>
+                                                <th scope="col">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>20.03$</td>
+                                                <td>Paypal</td>
+                                                <td>
+                                                    <span class="badge badge-success p-2"> <i
+                                                            class="fa-solid fa-check pr-1"></i> Sent </span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2</th>
+                                                <td>12.35$</td>
+                                                <td>Payeer</td>
+                                                <td>
+                                                    <span class="badge badge-warning p-2"> <i
+                                                            class="fa-solid fa-spinner pr-1"></i> Pending </span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3</th>
+                                                <td>10$</td>
+                                                <td>Vodafone</td>
+                                                <td>
+                                                    <span class="badge badge-danger p-2"> <i
+                                                            class="fa-solid fa-x pr-1"></i> Failed</span>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!---------------- Request withdrawals Modal ------------------>
+                                <div class="modal fade" id="createRequestModal" tabindex="-1" role="dialog"
+                                    aria-labelledby="createRequestLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="createRequestLabel"> <i
+                                                        class="fa-solid fa-plus-circle"></i>
+                                                    Create
+                                                    Request Withdrawals </h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form id="create-order" enctype="multipart/form-data">
+
+
+
+
+
+
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" id="create-contract" class="btn purple"> <i
+                                                        class="fa-solid fa-check"></i> Confirm </button>
+                                                <button type="button" class="btn cancle" data-dismiss="modal"> <i
+                                                        class="fa-solid fa-xmark"></i> Close </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+
+
+
+
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
